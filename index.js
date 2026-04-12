2dc5248aed786a5d1078699c4b938fbf379871d7
Marian Kristel <mkmk@Marians-MacBook-Air.local>
2026-04-12 23:02:28 +0800

    final fix render port

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Harry is alive 🧙‍♂️");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("🌐 Web server running");
-});
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
